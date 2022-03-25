618f0262d4e50abb6fef2549e4d6bd049207217e
KelvinLainez <101296285+KelvinLainez@users.noreply.github.com>
2022-03-25 12:05:13 -0600

    Add files via upload

---
+++ Convertidor.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package convertidor;
+
+
+
+/**
+ *
+ * @author HP
+ */
+public class Convertidor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      new frmConvertidor().setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
